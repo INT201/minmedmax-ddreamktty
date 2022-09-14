fa81b9ad364897840b74404973a2c6a9dddf2735
ddreamktty <kittiya.dreamktty@mail.kmutt.ac.th>
2022-09-14 16:30:22 +0700

    commitminmedmax

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,14 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+  let min = n1 < n2 && n1 < n3 ? n1 : n2 < n3 ? n2 : n3;
+  let max = n1 > n2 && n1 > n3 ? n1 : n2 > n3 ? n2 : n3;
+  let  arr = [n1, n2, n2];
+  arr.splice(arr.indexOf(min),1);
+  arr.splice(arr.indexOf(max),1);
+  let mid = arr[0];
+
+  return{
+    min : min, mid : mid, max : max};
 }
 module.exports = minMedMax
